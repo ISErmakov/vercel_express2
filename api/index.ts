d566181ee8a8e3d8396f202e6c674642137315af
Ermakovis <ermakov.ugra@gmail.com>
2024-10-11 22:56:35 +0500

    third6

--- api/index.ts
+++ api/index.ts
@@ -1,23 +1,13 @@
 import express from "express";
 
 const app = express();
-const port = process.env.PORT || 3003
+const port = process.env.PORT || 3005s
 
 
-app.get("/", (req, res) => {res.send("Express on Vercel 3003")});
-//app.get("/", (req, res) => res.send("Express on Vercel!!!!!!!!!!!"));
+app.get("/", (req, res) => {res.send("Express on Vercel ${port}")});
 
-app.listen(port, () => console.log("Server ready on port 3003WOW."));
-
-module.exports = app;
-
-/**
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
 module.exports = app;
- */
